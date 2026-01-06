381031f0c44f9e48ef591f01ca8eb3ea45acea55
Praveen V <praveenraju0011@gamil.com>
2026-01-06 23:57:24 +0530

    Implemented the main layout with content for hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 };
