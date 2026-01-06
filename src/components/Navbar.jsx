f18892d8990a68ccd00e422ed2906d89acfb36c2
Praveen V <praveenraju0011@gamil.com>
2026-01-06 15:44:34 +0530

    added nav bar and some assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { navLinks } from "../../Constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const gsapControl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    gsapControl.fromTo(
+      "nav",  
+      { background: "transparent" },
+      {
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo-img" />
+          <p>Velvet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map((item, index) => {
+            return (
+              <li key={item.id}>
+                <a href={`#${item.id}`}>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
